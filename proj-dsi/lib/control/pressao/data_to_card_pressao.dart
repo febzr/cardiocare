7523f5835d24fec07eddb83785d5d720b48af49b
Gabriel da Silva <137402778+Gabrielsilva090@users.noreply.github.com>
2024-07-27 21:46:06 -0300

    card_pressao controle

---
+++ proj-dsi/lib/control/pressao/data_to_card_pressao.dart
@@ -0,0 +1,14 @@
+import 'package:cardiocare/components/cards/card_pressao.dart';
+import 'package:cardiocare/model/model_card_pressao.dart';
+
+cardPressao dataToCardPressao(modelCardPressao data) {
+  final card = cardPressao(
+    sistole: data.sistole,
+    diastole: data.diastole,
+    pulso: data.pulso,
+    peso: data.peso,
+    datatime: data.datatime,
+    observacao: data.observacao,
+  );
+  return card;
+}
