abd24065f31f3c754aefcf2aa65315236f59fe49
Felipe Bezerra <136595371+febzr@users.noreply.github.com>
2024-08-16 12:14:58 -0300

    Criação do custom_adicionar e modificações no display

---
+++ proj-dsi/lib/components/buttons/custom_adicionar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class customAdicionar extends StatelessWidget {
+  final Function data;
+  final double height;
+  final double width;
+  const customAdicionar({
+    super.key,
+    required this.data,
+    required this.height,
+    required this.width,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * width,
+      height: MediaQuery.of(context).size.height * height,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+        onPressed: () {
+          data();
+        },
+        child: Text(
+          '+ Adicionar',
+          style: Theme.of(context).textTheme.labelSmall,
+        ),
+      ),
+    );
+  }
+}
--- proj-dsi/lib/components/display/display_large.dart
+++ proj-dsi/lib/components/display/display_large.dart
@@ -8,26 +8,37 @@ class displayLarge extends StatelessWidget{
 
   
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: this.ontap,
-      child: Stack(
-        alignment: Alignment.bottomLeft,
-        children: [
-          Container(
-            width: (178/430)*MediaQuery.of(context).size.width,
-            height: (207/932)*MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(borderRadius:  BorderRadius.circular(20)),
-            child: 
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  
-                  child: Image.asset(this.image,fit: BoxFit.cover,)
+    return Material(
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: this.ontap,
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            Container(
+              width: (190/430)*MediaQuery.of(context).size.width,
+              height: (207/932)*MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(borderRadius:  BorderRadius.circular(20),boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.25), 
+                 
+                  blurRadius: 4,
+                  offset: Offset(0, 4)
                 ),
-          ),Container(margin: EdgeInsets.only(left:10,bottom: 17),
-          width: (139/430)*MediaQuery.of(context).size.width,
-            child: Text(this.label,style: Theme.of(context).textTheme.displayMedium,)
-          )
-        ]
+              ],),
+              child: 
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    
+                    child: Image.asset(this.image,fit: BoxFit.cover,)
+                  ),
+            ),Container(margin: EdgeInsets.only(left:10,bottom: 17),
+            width: (139/430)*MediaQuery.of(context).size.width,
+              child: Text(this.label,style: Theme.of(context).textTheme.displayMedium,)
+            )
+          ]
+        )
       )
     );
   }
