bf1b89dc2ccc347955ef294552aff41bf5f87250
Raphael Cordeiro <153565787+rapha-cb@users.noreply.github.com>
2024-08-17 12:44:15 -0300

    Create custom_remedio_picker.dart

---
+++ proj-dsi/lib/components/inputs/custom_remedio_picker.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class customRemedioPicker extends StatefulWidget {
+  final String hint;
+  final String? Function(String?) validador;
+  final TextEditingController controller;
+  final double height;
+  final double width;
+
+  const customRemedioPicker(
+      {super.key,
+      required this.validador,
+      required this.hint,
+      required this.controller,
+      required this.height,
+      required this.width});
+
+  State<customRemedioPicker> createState() => _customTextField();
+}
+
+class _customTextField extends State<customRemedioPicker> {
+  bool labelcor = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: (341 / 430) * MediaQuery.of(context).size.width,
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          TextFormField(
+            controller: widget.controller,
+            validator: (value) {
+              String? result = widget.validador(value);
+              if (result == null) {
+                setState(() {
+                  labelcor = false;
+                });
+              } else {
+                setState(() {
+                  labelcor = true;
+                });
+              }
+              return result;
+            },
+            style: Theme.of(context).textTheme.labelMedium,
+            decoration: InputDecoration(
+                fillColor: Theme.of(context).cardColor,
+                filled: true,
+                hintStyle: Theme.of(context).textTheme.labelMedium,
+                hintText: widget.hint,
+                enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Theme.of(context).primaryColor),
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Theme.of(context).primaryColor),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                errorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Theme.of(context).primaryColor,width: 2),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Theme.of(context).primaryColor,width: 2),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  
+                ),
+                contentPadding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.024,
+                    bottom: MediaQuery.of(context).size.height * 0.024,
+                    left: MediaQuery.of(context).size.width * 0.044)),
+          )
+        ]));
+  }
+}
