a9842b2b6bf7f049d0723adc3ca88fb8a357508c
khadidjamoraes <khadidja.developer@gmail.com>
2024-08-21 15:48:31 -0300

    diario de pressao modificado (CRUD)

--- proj-dsi/lib/view/diario_de_pressao.dart
+++ proj-dsi/lib/view/diario_de_pressao.dart
@@ -1,28 +1,58 @@
 import 'package:cardiocare/components/buttons/back_button.dart';
 import 'package:cardiocare/components/buttons/custom_Floating_Action_Buttom.dart';
+import 'package:cardiocare/components/cards/card_pressao.dart';
 import 'package:cardiocare/components/inputs/custom_data_picker_null.dart';
-
-
-
-
+import 'package:cardiocare/control/utils/is_dataigual.dart';
 import '../model/teste_cards.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class diariodepressao extends StatefulWidget {
-  diariodepressao({super.key});
+  const diariodepressao({super.key});
 
+  @override
   State<diariodepressao> createState() => _diarioDePressao();
 }
 
 class _diarioDePressao extends State<diariodepressao> {
   @override
+  DateTime? data;
+
+  setsetstate(cardPressao i){
+      i.state=(){
+      setState(() {
+        
+      });
+
+      
+
+    };
+    return i;
+
+  }
+  bool dataisigual(cardPressao i, DateTime? d) {
+    DateTime t = i.getdatetime();
+
+    final r = isDataIgual(datetime1: t, datetime2: d);
+    return r.isigual();
+  }
+
   Widget build(BuildContext context) {
-    DateTime? data = null;
     return Scaffold(
-      floatingActionButton: customFloatingActionbutton(onpress: (){Navigator.of(context).pushNamed('criador_de_pressao');},),
-      body: Container(
+      floatingActionButton: customFloatingActionbutton(icone: Icon(
+            Icons.add_rounded,
+
+            size: MediaQuery.of(context).size.width * (48 / 430),
+            color: Colors.white,
+          ),
+        onpress: () {
+          Navigator.of(context).pushNamed('criador_de_pressao').then((_) {
+  setState(() {
+  });
+});;
+          
+        },
+      ),
+      body: SizedBox(
         width: double.infinity,
         child: SafeArea(
           child: Column(
@@ -55,18 +85,21 @@ class _diarioDePressao extends State<diariodepressao> {
                     child: customDatePickerNull(
                       onchange: (picker) {
                         data = picker;
+                        setState(() {
+                          data = picker;
+                        });
                       },
                     ))
               ]),
               Expanded(
-                child: Container(
+                child: SizedBox(
                     width: (374 / 430) * MediaQuery.of(context).size.width,
                     child: FutureBuilder(
                         future: listaCardPressao(),
                         builder: ((context, snapshot) {
                           if (snapshot.connectionState ==
                               ConnectionState.waiting) {
-                            return CircularProgressIndicator();
+                            return const CircularProgressIndicator();
                           } else if (snapshot.hasError) {
                             return Text('Erro: ${snapshot.error}');
                           } else {
@@ -75,7 +108,12 @@ class _diarioDePressao extends State<diariodepressao> {
                                   setState(() {});
                                 },
                                 child: ListView(children: [
-                                  for (var i in snapshot.data!) i
+                                  for (var i in snapshot.data!)
+                                    (data == null)
+                                        ? setsetstate(i)
+                                        : (dataisigual(setsetstate(i), data)
+                                            ? setsetstate(i)
+                                            : SizedBox.shrink())
                                 ]));
                           }
                         }))),
