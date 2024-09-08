c20bc70daf0d5229a849f7ce71558ce756949032
Yago-Ferraz <yagomouraferraz100@gmail.com>
2024-09-08 08:00:27 -0300

    'firebase'

--- proj-dsi/lib/main.dart
+++ proj-dsi/lib/main.dart
@@ -1,7 +1,14 @@
 import 'themes/theme_main.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
